089705a5ed686fb95d86ae6a1d0252f8faa7d793
Vincent Bonmalais <vb.kouno@gmail.com>
2014-04-13 09:27:34 +0100

    ReRaise on backtick failure too

--- lib/re-raise.rb
+++ lib/re-raise.rb
@@ -11,15 +11,28 @@ class ReRaise
   def self.enable
     Kernel.class_eval do
       def system_with_raise(*args)
-        result = system_old(*args)
+        system_old(*args).tap do
+          raise_if_wrong_exit_code
+        end
+      end
 
-        unless result
-          raise SystemExitError, $?.exitstatus, "Sytem error returned code #{args.first}"
+      def backtick_with_raise(*args)
+        old_backtick(*args).tap do
+          raise_if_wrong_exit_code
+        end
+      end
+
+      def raise_if_wrong_exit_code
+        unless $?.exitstatus == 0
+          raise SystemExitError, $?.exitstatus, "System error returned code (#{$?.exitstatus})"
         end
       end
 
       alias_method :system_old, :system
       alias_method :system, :system_with_raise
+
+      alias_method :old_backtick, :`
+      alias_method :`, :backtick_with_raise
     end
   end
 end
--- spec/integration/reraise_spec.rb
+++ spec/integration/reraise_spec.rb
@@ -6,32 +6,50 @@ describe ReRaise do
       ReRaise.enable
     end
 
+    shared_examples 'a system command' do
+      context 'when the program returns false' do
+        it 'raises an exception' do
+          expect { subject.call('false') }.to raise_error
+        end
+      end
+
+      context 'when the program have a non-zero exit code' do
+        it 'raises an exception' do
+          expect { subject.call('exit 1') }.to raise_error
+        end
+
+        it 'contains the error code' do
+          begin
+            subject.call('exit 12')
+          rescue ReRaise::SystemExitError => e
+            expect(e.exitstatus).to eq(12)
+          end
+        end
+      end
+    end
+
+    describe 'Kernel::system' do
+      subject do
+        Proc.new { |*args| system(*args) }
+      end
+
+      it_behaves_like 'a system command'
+    end
+
+    describe 'Kernel::`' do
+      subject do
+        Proc.new { |*args| `#{args.join('')}` }
+      end
+
+      it_behaves_like 'a system command'
+    end
+
     it 'can be re-enabled safely' do
       pending
     end
 
     it 'overwrites backtick (`command`)' do
       pending
-    end
-
-    context 'when the program returns false' do
-      it 'raises an exception' do
-        expect { system('false') }.to raise_error
-      end
-    end
-
-    context 'when the program have a non-zero exit code' do
-      it 'raises an exception' do
-        expect { system('exit 1') }.to raise_error
-      end
-
-      it 'contains the error code' do
-        begin
-          system('exit 12')
-        rescue ReRaise::SystemExitError => e
-          expect(e.exitstatus).to eq(12)
-        end
-      end
     end
   end
 end
