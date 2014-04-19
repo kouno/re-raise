d73594e89dbb5f6d56db0b1e789d8812239934a6
Vincent Bonmalais <vb.kouno@gmail.com>
2014-04-19 15:33:52 +0100

    Enable and disable re-raise safely

--- lib/re-raise.rb
+++ lib/re-raise.rb
@@ -18,23 +18,49 @@ module ReRaise
 
   def self.enable
     Kernel.class_eval do
-      def system_with_raise(*args)
-        system_old(*args).tap do
-          ReRaise.raise_if_wrong_exit_code
+      unless respond_to?(:system_with_raise)
+        def system_with_raise(*args)
+          system_old(*args).tap do
+            ReRaise.raise_if_wrong_exit_code
+          end
         end
+
+        alias_method :system_old, :system
+        alias_method :system, :system_with_raise
       end
 
-      def backtick_with_raise(*args)
-        old_backtick(*args).tap do
-          ReRaise.raise_if_wrong_exit_code
+      unless respond_to?(:backtick_with_raise)
+        def backtick_with_raise(*args)
+          old_backtick(*args).tap do
+            ReRaise.raise_if_wrong_exit_code
+          end
         end
+
+        alias_method :old_backtick, :`
+        alias_method :`, :backtick_with_raise
+      end
+    end
+  end
+
+  def self.disable
+    Kernel.class_eval do
+      if respond_to?(:system_old, true)
+        alias_method :system, :system_old
+        remove_method(:system_old)
       end
 
-      alias_method :system_old, :system
-      alias_method :system, :system_with_raise
+      if respond_to?(:system_with_raise, true)
+        remove_method(:system_with_raise)
+      end
 
-      alias_method :old_backtick, :`
-      alias_method :`, :backtick_with_raise
+      if respond_to?(:old_backtick, true)
+        alias_method :`, :old_backtick
+        remove_method(:old_backtick)
+      end
+
+      if respond_to?(:backtick_with_raise, true)
+        remove_method(:backtick_with_raise)
+      end
     end
   end
 end
--- spec/integration/reraise_spec.rb
+++ spec/integration/reraise_spec.rb
@@ -1,31 +1,31 @@
 require 'spec_helper'
 
 describe ReRaise do
+  shared_examples 'a system command' do
+    context 'when the program returns false' do
+      it 'raises an exception' do
+        expect { subject.call('false') }.to raise_error
+      end
+    end
+
+    context 'when the program have a non-zero exit code' do
+      it 'raises an exception' do
+        expect { subject.call('exit 1') }.to raise_error
+      end
+
+      it 'contains the error code' do
+        begin
+          subject.call('exit 12')
+        rescue ReRaise::SystemExitError => e
+          expect(e.exitstatus).to eq(12)
+        end
+      end
+    end
+  end
+
   context 'when it is enabled' do
     before(:all) do
       ReRaise.enable
-    end
-
-    shared_examples 'a system command' do
-      context 'when the program returns false' do
-        it 'raises an exception' do
-          expect { subject.call('false') }.to raise_error
-        end
-      end
-
-      context 'when the program have a non-zero exit code' do
-        it 'raises an exception' do
-          expect { subject.call('exit 1') }.to raise_error
-        end
-
-        it 'contains the error code' do
-          begin
-            subject.call('exit 12')
-          rescue ReRaise::SystemExitError => e
-            expect(e.exitstatus).to eq(12)
-          end
-        end
-      end
     end
 
     describe 'Kernel::system' do
@@ -43,9 +43,38 @@ describe ReRaise do
 
       it_behaves_like 'a system command'
     end
+  end
 
-    it 'can be re-enabled safely' do
-      pending
+  context 'when switched ON multiple times' do
+    before do
+      3.times { ReRaise.enable }
+    end
+
+    describe 'Kernel::system' do
+      subject do
+        Proc.new { |*args| system(*args) }
+      end
+
+      it_behaves_like 'a system command'
+    end
+
+    describe 'Kernel::`' do
+      subject do
+        Proc.new { |*args| `#{args.join('')}` }
+      end
+
+      it_behaves_like 'a system command'
+    end
+  end
+
+  context 'when switched OFF' do
+    before do
+      ReRaise.enable
+      ReRaise.disable
+    end
+
+    it 'has Kernel default behavior' do
+      expect { system('exit 1') }.to_not raise_error
     end
   end
 end
