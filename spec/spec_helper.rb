78943a5005012c5bc11f865786b2546aa3530883
Vincent Bonmalais <vb.kouno@gmail.com>
2014-08-25 14:57:45 +0100

    Configure Rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,15 @@
 require_relative '../lib/re-raise'
+
+RSpec.configure do |config|
+  config.default_formatter = 'doc'
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+
+    mocks.verify_partial_doubles = true
+  end
+end
